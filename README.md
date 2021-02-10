b86de7aabd80dbf83c7898d19074ef3f6244cbd0
Kayla <kay_kay1424@yahoo.com>
2021-02-09 19:40:16 -0600

    Fix src of example image and add link to app in README

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Markdown Previewer
+# [Markdown Previewer](https://kaykay1424.github.io/markdown-previewer/)
 
 This app lets you type in some markdown and preview the output.
 
--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import './index.scss';
 
 class App extends Component {
   state = {
-        markdown: "# This is a heading \n\n## This a sub-heading\n\nHere is some **bold** text and _italic_ text\n\n This text is ~~crossed out~~\n\n[This is a link]( https://goo.gl/Umyytc)\n\nHere is some inline code: `<div></div>`\n\nHere is a code block:\n```\nfunction anotherExample(firstLine, lastLine) { \n if (firstLine == '```' && lastLine == '```') {\n    return multiLineCode;\n   }\n}\n``` \n\n > Here is a block quote \n\n Here is an unordered list: \n\n - Bullet point \n  - Another bullet point\n    - Another bullet point\n\nHere is an ordered list:\n\n 1. List item #1\n 1. List item #2\n 1. List item #3\n\nHere is a table: \n\n Header 1 | Header 2 \n ----- | ----- \n Cell 1 | Cell 2 \n\nHere is an image:\n\n![React Logo w/ Text](/favicon.ico)"
+        markdown: "# This is a heading \n\n## This a sub-heading\n\nHere is some **bold** text and _italic_ text\n\n This text is ~~crossed out~~\n\n[This is a link]( https://goo.gl/Umyytc)\n\nHere is some inline code: `<div></div>`\n\nHere is a code block:\n```\nfunction anotherExample(firstLine, lastLine) { \n if (firstLine == '```' && lastLine == '```') {\n    return multiLineCode;\n   }\n}\n``` \n\n > Here is a block quote \n\n Here is an unordered list: \n\n - Bullet point \n  - Another bullet point\n    - Another bullet point\n\nHere is an ordered list:\n\n 1. List item #1\n 1. List item #2\n 1. List item #3\n\nHere is a table: \n\n Header 1 | Header 2 \n ----- | ----- \n Cell 1 | Cell 2 \n\nHere is an image:\n\n![React Logo w/ Text]("+process.env.PUBLIC_URL+"/favicon.ico)"
   }
   
   createMarkedText = () => {
